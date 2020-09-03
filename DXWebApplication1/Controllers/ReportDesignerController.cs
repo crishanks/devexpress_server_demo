cb25c2a3673373bf9e67b1aa6d8d542b3a381a52
Cris Hanks <I27761@xactware.com>
2020-09-03 10:21:16 -0600

    working designer for demo

---
+++ DXWebApplication1/Controllers/ReportDesignerController.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.DataAccess.Sql;
+using DevExpress.Web.Mvc.Controllers;
+using DevExpress.XtraReports.Web.ReportDesigner;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace DXWebApplication1.Controllers
+{
+  public class ReportDesignerController : ReportDesignerApiController
+  {
+        public override ActionResult Invoke()
+        {
+            var result = base.Invoke();
+            Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            return result;
+        }
+
+        public ActionResult GetReportDesignerModel(string reportUrl)
+      {
+          Response.AppendHeader("Access-Control-Allow-Origin", "*");
+
+          string modelJsonScript =
+              new ReportDesignerClientSideModelGenerator()
+              .GetJsonModelScript(
+                  reportUrl,                 // The URL of a report that is opened in the Report Designer when the application starts.
+                  GetAvailableDataSources(), // Available data sources in the Report Designer that can be added to reports.
+                  "ReportDesigner/Invoke",   // The URI path of the controller action that processes requests from the Report Designer.
+                  "WebDocumentViewer/Invoke",// The URI path of the controller action that processes requests from the Web Document Viewer.
+                  "QueryBuilder/Invoke"      // The URI path of the controller action that processes requests from the Query Builder.
+              );
+          return Content(modelJsonScript, "application/json");
+      }
+
+      Dictionary<string, object> GetAvailableDataSources()
+      {
+          //SqlConnection connection = new SqlConnection("data source=(localdb)/mssqllocaldb;initial catalog=aspnet-DXWebApplication1-20200900153700;integrated security=SSPI");
+          //connection.Open();
+          var dataSources = new Dictionary<string, object>();
+          SqlDataSource ds = new SqlDataSource("Northwind_Connection");
+          var productsQuery = SelectQueryFluentBuilder.AddTable("Products").SelectAllColumns().Build("Products");
+          var customersQuery = SelectQueryFluentBuilder.AddTable("Customers").SelectAllColumns().Build("Customers");
+          ds.Queries.Add(productsQuery);
+          ds.Queries.Add(customersQuery);
+          ds.RebuildResultSchema();
+          dataSources.Add("SqlDataSource", ds);
+          return dataSources;
+      }
+  }
+}
---
+++ DXWebApplication1/ReportStorageWebExtension1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraReports.Web.Extensions;
+
+namespace DXWebApplication1
+{
+    public class ReportStorageWebExtension1 : DevExpress.XtraReports.Web.Extensions.ReportStorageWebExtension
+    {
+        public Dictionary<string, XtraReport> Reports = new Dictionary<string, XtraReport>();
+
+        public ReportStorageWebExtension1()
+        {
+            Reports.Add("Products", new XtraReport1());
+        }
+
+        public override bool CanSetData(string url)
+        {
+            return base.CanSetData(url);
+        }
+        public override bool IsValidUrl(string url)
+        {
+            return base.IsValidUrl(url);
+        }
+
+        public override byte[] GetData(string url)
+        {
+            return base.GetData(url);
+        }
+
+        public override Dictionary<string, string> GetUrls()
+        {
+            return Reports.ToDictionary(x => x.Key, y => y.Key);
+        }
+
+        public override void SetData(DevExpress.XtraReports.UI.XtraReport report, string url)
+        {
+            if (Reports.ContainsKey(url))
+            {
+                Reports[url] = report;
+            }
+            else
+            {
+                Reports.Add(url, report);
+            }
+        }
+
+        public override string SetNewData(DevExpress.XtraReports.UI.XtraReport report, string defaultUrl)
+        {
+            SetData(report, defaultUrl);
+            //return base.SetNewData(report, defaultUrl);
+            return defaultUrl;
+        }
+    }
+}
